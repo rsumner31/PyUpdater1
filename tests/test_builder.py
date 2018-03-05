6377e63641b4b3036c4f519016dd403bf312931f
Robert Sumner Jr <rsumner31@gmail.com>
2018-03-04 22:01:33 -0600

    Attempt to fix package_updater
    Conflicts:
    	pyupdater/package_handler.py
    	pyupdater/utils/package.py
    	tests/test_builder.py

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -24,7 +24,6 @@ from pyupdater.wrapper.builder import Builder
 from pyupdater.wrapper.options import get_parser
 from tconfig import TConfig
 
-new_folder = os.path.join('pyu-data', 'new')
 
 @pytest.mark.usefixtures('cleandir')
 class TestNoRepo(object):
@@ -43,7 +42,7 @@ class TestMakeSpec(object):
         t_config.DATA_DIR = os.getcwd()
         pyu = PyUpdater(t_config)
         pyu.setup()
-
+        new_folder = os.path.join('pyu-data', 'new')
         spec_cmd = ['make-spec', 'app.py', '-F', '--app-name', 'MyApp',
                     '--app-version', '0.1.0']
         spec_file_name = get_system() + '.spec'
@@ -73,7 +72,7 @@ class TestBuild(object):
         t_config.DATA_DIR = os.getcwd()
         pyu = PyUpdater(t_config)
         pyu.setup()
-
+        new_folder = os.path.join('pyu-data', 'new')
         build_cmd = ['build', '--app-name', 'MyApp',
                      '--app-version', '0.1.0', 'app.py']
         build_cmd = [str(b) for b in build_cmd]
@@ -109,29 +108,21 @@ class TestBuild(object):
         t_config.DATA_DIR = os.getcwd()
         pyu = PyUpdater(t_config)
         pyu.setup()
-
-        spec_cmd = ['make-spec', 'app.py', '-F',
-                    '--app-name', 'MyApp',
-                    '--app-version', '0.1.0']
-        spec_file_name = get_system() + '.spec'
-        build_cmd = ['build', '--clean', '--app-name', 'MyApp',
-                     '--app-version', '0.1.0', spec_file_name]
-
+        new_folder = os.path.join('pyu-data', 'new')
+        build_cmd = ['build', '-F', '-w', '--app-name', 'MyApp',
+                     '--app-version', '0.1.0', 'app.py']
+        build_cmd = [str(b) for b in build_cmd]
         parser = get_parser()
         with open('app.py', 'w') as f:
-            # Missing closing quote
             f.write('print "Hello, World!"')
-        args, pyu_args = parser.parse_known_args(spec_cmd)
-        b = Builder(args, pyu_args)
-        b.make_spec()
-        assert os.path.exists(spec_file_name)
         args, pyu_args = parser.parse_known_args(build_cmd)
         b = Builder(args, pyu_args)
         b.build()
         with ChDir(new_folder):
             assert len(os.listdir(os.getcwd())) == 1
+>>>>>>> parent of c5ab548... Added builder tests
 
-    def test_build_fail_script_syntax_error(self):
+    def test_build_fail(self):
         with pytest.raises(SystemExit):
             t_config = TConfig()
             t_config.DATA_DIR = os.getcwd()
@@ -152,18 +143,3 @@ class TestBuild(object):
             assert os.path.exists(spec_file_name)
             args, pyu_args = parser.parse_known_args(build_cmd)
             b = Builder(args, pyu_args)
-
-
-    def test_build_fail_no_spec_or_src(self):
-        with pytest.raises(SystemExit):
-            t_config = TConfig()
-            t_config.DATA_DIR = os.getcwd()
-            pyu = PyUpdater(t_config)
-            pyu.setup()
-            build_cmd = ['build', '--app-name', 'MyApp', '--clean'
-                         '--app-version', '0.1.0', '-F']
-
-            parser = get_parser()
-            args, pyu_args = parser.parse_known_args(build_cmd)
-            b = Builder(args, pyu_args)
-            b.build()
