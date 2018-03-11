1b8bb2dfcf200d17d5d1137d74a9d24be43f2c98
Robert Sumner Jr <rsumner31@gmail.com>
2018-03-11 08:00:16 -0500

    renamed
    Conflicts:
    	demos/client.py
    	pyi_updater/__init__.py
    	pyi_updater/_version.py
    	pyi_updater/client/__init__.py
    	pyi_updater/client/downloader.py
    	pyi_updater/client/patcher.py
    	pyi_updater/client/updates.py
    	pyi_updater/core.py
    	pyi_updater/hooks/__init__.py
    	pyi_updater/hooks/hook-certifi.py
    	pyi_updater/hooks/hook-jms_utils.py
    	pyi_updater/hooks/hook-requests.py
    	pyi_updater/settings.py
    	pyi_updater/wrapper/__init__.py
    	pyi_updater/wrapper/builder.py
    	pyi_updater/wrapper/options.py
    	pyupdater/__init__.py
    	pyupdater/_version.py
    	pyupdater/client/__init__.py
    	pyupdater/client/downloader.py
    	pyupdater/client/patcher.py
    	pyupdater/client/updates.py
    	pyupdater/core.py
    	pyupdater/hooks/__init__.py
    	pyupdater/hooks/hook-certifi.py
    	pyupdater/hooks/hook-jms_utils.py
    	pyupdater/hooks/hook-requests.py
    	pyupdater/settings.py
    	pyupdater/wrapper/__init__.py
    	pyupdater/wrapper/builder.py
    	pyupdater/wrapper/options.py
    	setup.py

--- pyupdater/uploader/common.py
+++ pyupdater/uploader/common.py
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # --------------------------------------------------------------------------
-import os
-
-
-def get_hook_dir():
-    return os.path.abspath(os.path.dirname(__file__))
+# ToDo: Remove in v0.22
+from pyupdater.uploader import BaseUploader
+# End ToDo
